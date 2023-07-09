90c14cf9a45af99c66107854e8a77b9b369e2647
ArtemB118 <1evkakii1@gmail.com>
2023-07-09 16:12:41 +0600

    Divide func

--- savepthoto.py
+++ savepthoto.py
@@ -7,7 +7,64 @@ with open ('Ya_token.txt', 'r') as file:
 with open ('VK_token.txt', 'r') as file:
     vk_token = file.readline()
 
-def backup_vk_profile_photos(user_id, token_yandex_disk, token_VK, count=999):
+upload_url = "https://cloud-api.yandex.net/v1/disk/resources/upload"
+
+def get_vk_profile_photos(user_id, token_VK, count=999):
+    """
+    Получает фотографии с профиля VK.
+    Возвращает список фотографий или None в случае ошибки.
+    """
+    response = requests.get(
+        f"https://api.vk.com/method/photos.get?owner_id={user_id}&album_id=profile&count={count}&access_token={token_VK}&v=5.131"
+    )
+    data = response.json()
+
+    if "error" in data:
+        error_msg = data["error"]["error_msg"]
+        print(f"Ошибка при получении фотографий с профиля VK: {error_msg}")
+        return None
+
+    photos = data["response"]["items"]
+    return photos
+
+def create_yandex_disk_folder(folder_name, token_yandex_disk):
+    """
+    Создает папку на Яндекс.Диске.
+    Возвращает True, если папка успешно создана, иначе False.
+    """
+    headers = {
+        "Authorization": f"OAuth {token_yandex_disk}",
+    }
+    params = {"path": folder_name}
+    response = requests.put("https://cloud-api.yandex.net/v1/disk/resources", headers=headers, params=params)
+
+    if response.status_code == 201:
+        print(f"Папка {folder_name} успешно создана на Яндекс.Диске")
+        return True
+    else:
+        print(f"Ошибка при создании папки {folder_name} на Яндекс.Диске")
+        return False
+
+def upload_photo_to_yandex_disk(file_path, upload_url, token_yandex_disk):
+    """
+    Загружает фотографию на Яндекс.Диск.
+    Возвращает True, если загрузка прошла успешно, иначе False.
+    """
+    headers = {
+        "Authorization": f"OAuth {token_yandex_disk}",
+    }
+    with open(file_path, "rb") as f:
+        response = requests.put(upload_url, headers=headers, files={"file": f})
+
+    if response.status_code == 201:
+        print(f"Фотография {file_path} успешно загружена на Яндекс.Диск")
+        return True
+    else:
+        print(f"Ошибка при загрузке фотографии {file_path} на Яндекс.Диск")
+        return False
+
+
+# def backup_vk_profile_photos(user_id, token_yandex_disk, token_VK, count=999):
     # Получение фотографий с профиля VK
     response = requests.get(
         f"https://api.vk.com/method/photos.get?owner_id={user_id}&album_id=profile&count={count}&access_token={token_VK}&v=5.131"
@@ -77,4 +134,3 @@ def backup_vk_profile_photos(user_id, token_yandex_disk, token_VK, count=999):
 
     print("Все фотографии успешно сохранены и загружены на Яндекс.Диск")
 
-backup_vk_profile_photos(172843154, ya_token, vk_token)
